9a6281bdbbed27f2104dd5b957ba60e881340853
Billy19191 <bill.ratchanon@gmail.com>
2025-04-07 19:10:35 +0700

    ✨ feat: hot-reload air

--- main.go
+++ main.go
@@ -6,13 +6,24 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+type Test struct {
+	Message string `json:"message"`
+	Status  string `json:"status"`
+}
+
 func main() {
 	fmt.Println("hello world")
 
-	app := fiber.New()
+	app := fiber.New(fiber.Config{
+		Network: fiber.NetworkTCP,
+	})
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("hello world 🌈")
+		// return c.SendString("hello world 🌈")
+		return c.JSON(Test{
+			Message: "hello world",
+			Status:  "success",
+		})
 	})
 
 	app.Listen(":3000")
